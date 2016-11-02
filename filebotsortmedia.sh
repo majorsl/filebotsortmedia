fa724d153a4b3488895600b0b3ea97bd79ea6e21
majorsl <steve@themajorshome.com>
2016-11-01 20:35:44 -0400

    Added .lnk files to clean up list.

--- filebotsortmedia.sh
+++ filebotsortmedia.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# version 2.5.7 *REQUIREMENTS BELOW*
+# version 2.5.8 *REQUIREMENTS BELOW*
 #
 # 1. Working Homebrew installed.
 # 2. Homebrew: brew tap caskroom/cask
@@ -154,7 +154,7 @@ do
 done
 
 # clean up these files so they don't get moved to the show directories.
-filearray=( '*.nfo' '.DS_Store' '*.srt' '*.sfv' '*.jpg' '*.idx' '*.md5' '*.url' '*.mta' '*.txt' '*.png' '*.ico' '*.xml' '*.htm' '.html' '*.web' '*.website' '*.torrent' '*.sql' '*.sql-lite' 'Thumbs.db' )
+filearray=( '*.nfo' '.DS_Store' '*.srt' '*.sfv' '*.jpg' '*.idx' '*.md5' '*.url' '*.mta' '*.txt' '*.png' '*.ico' '*.xml' '*.htm' '.html' '*.web' '*.lnk' '*.website' '*.torrent' '*.sql' '*.sql-lite' 'Thumbs.db' )
 
 for delfile in "${filearray[@]}"
 do
